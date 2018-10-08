93d3f0be38e8ebd1baa5e0f7b952e999bc963b95
Bram Strack van Schijndel <bramsvs@gmail.com>
2018-10-08 20:55:45 +0200

    fix

--- src/modules/mc_att_control/mc_att_control.hpp
+++ src/modules/mc_att_control/mc_att_control.hpp
@@ -238,7 +238,6 @@ private:
 		(ParamFloat<px4::params::SENS_BOARD_Y_OFF>) _board_offset_y,
 		(ParamFloat<px4::params::SENS_BOARD_Z_OFF>) _board_offset_z,
 
-		(ParamFloat<px4::params::VT_WV_YAWR_SCL>) _vtol_wv_yaw_rate_scale,		/**< Scale value [0, 1] for yaw rate setpoint  */
 		(ParamFloat<px4::params::MC_ATT_RATE_MAX>) _att_rate_sample_rate_max
 	)
 
